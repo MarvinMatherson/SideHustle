75aded56d327b277fa02e6fd86f7d35b7df2661a
MarvinMatherson <55834622+MarvinMatherson@users.noreply.github.com>
2020-01-17 06:25:48 +0000

    we be too responsive thoooooo

--- script.js
+++ script.js
@@ -13,7 +13,6 @@ var apikey = "7975291d9b983f7792c4307b9d279ed0";
 var temp = 'celcius today';
 
 
-
  $.getJSON("http://api.weatherstack.com/current?access_key="+apikey+"&query="+latitude+"," +longitude, function(data){
   console.log(data);
 
@@ -23,15 +22,35 @@ var temp = 'celcius today';
   var location = data.location.name;
   console.log(location)
 
+  var country = data.location.country;
+  console.log(country)
+
   var wicon = data.current.weather_icons["0"];
   console.log(wicon)
 
-$('.temperture').html("The temperature in "+location+" is " +temperature+ " celcius ");
+//This is how to get the current time in a variable
+var today = new Date();
+var hourNow = today.getHours();
+
+//This is howe to display a different message depending on ther time of day
+var greeting;
+if (hourNow > 18){
+  greeting = 'Good evening, ';
+} else if (hourNow > 12){
+  greeting = 'Good afternoon, ';
+} else if (hourNow > 0) {
+  greeting = 'Good morning, ';
+}
+
+console.log("We gonna say " +greeting+ "at this time");
+
+$('.temperture').html(greeting+ "the temperature in "+location+", "+country+" is " +temperature+ " celcius. ");
 //IM GETTING FUCKING PISSED OFF WHY WONT THE IMAGE GO ONTO THE HTML
 
 $('#icon').append("<img src='"+wicon+"'></img>");
 
 
+
 });
 });
-})
+});
